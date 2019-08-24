39fdb55a0c6456b63d35566dab90a7afd7fa9ea4
Nako <Toxa23A@gmail.com>
2019-08-24 15:33:06 +0300

    sign up credentials validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { MinLength, MaxLength, IsString, Matches } from 'class-validator';
+
+// TODO: research in best practices for password regex
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(30)
+  @Matches(/((?=.*\d)|(?=.*\w+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
   password: string;
 }
